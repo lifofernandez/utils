b7cd5372d65dd930fba8481d714d7592997fb3f8
lifofernandez <lifofernandez@gmail.com>
2018-08-27 12:46:56 -0300

    minor changes

--- WAVs2MP3s.sh
+++ WAVs2MP3s.sh
@@ -4,22 +4,13 @@
 
 min_dur=60
 
-norm_st1="I=-16:TP=-1.5:LRA=11:measured_I=-27.61:measured_LRA=18.06:"
-norm_st2="measured_TP=-4.47:measured_thresh=-39.20:offset=0.58:"
-norm_st3="linear=true:print_format=summary"
-norm_st="$norm_st1$nomr_st2$norm_st3"
-# ESTUDIAR: http://k.ylo.ph/2016/04/04/loudnorm.html
-#-af loudnorm="$norm_st" \
-
-
 for f in $@
 do
-	echo "$f"
 	if [  ${f: -4} == ".wav" ]; then
 		duration=$(soxi -D $f)
 		long_enough=$(bc <<< "$duration > $min_dur")
 		if [ $long_enough -eq 1 ]; then
-			echo "\n### Procesando: $f ###\n"
+			printf "\n##### PROCESANDO: $f #####\n"
 			ffmpeg -i "$f" \
 			-af loudnorm \
 			-ar 48k \
@@ -30,3 +21,9 @@ do
 done
 
 
+#norm_st1="I=-16:TP=-1.5:LRA=11:measured_I=-27.61:measured_LRA=18.06:"
+#norm_st2="measured_TP=-4.47:measured_thresh=-39.20:offset=0.58:"
+#norm_st3="linear=true:print_format=summary"
+#norm_st="$norm_st1$nomr_st2$norm_st3"
+# ESTUDIAR: http://k.ylo.ph/2016/04/04/loudnorm.html
+#-af loudnorm="$norm_st" \
